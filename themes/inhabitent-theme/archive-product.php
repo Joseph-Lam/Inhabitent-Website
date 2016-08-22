ebd4dd71fe539458db5c07bd473d743dbfeb2ef6
Joseph-Lam <joseph.lam91@gmail.com>
2016-08-22 00:45:34 -0700

    finished shop page. Could not add price tag into box

--- themes/inhabitent-theme/archive-product.php
+++ themes/inhabitent-theme/archive-product.php
@@ -8,21 +8,38 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
+	
+	<h2 class="front-titles">Shop Stuff</h2>
+		
+		<?php 
+		$category_arr = array('taxonomy' => 'product-type', 'hide_empty' => true);
+
+		$shop_category = get_terms( $category_arr );?>
+		
+		<div class="shop-header-links">
+		<?php 
+		foreach ( $shop_category as $term ): ?> 
+			<a href="<?php echo get_term_link($term) ?>">
+			<?php echo $term->name ;?></a>
+		<?php endforeach;?>
+		</div>
+
 	<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<div class="shop-archive-product">
 
+		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
+					<header class="shop-grid-item">
 						<?php if ( has_post_thumbnail() ) : ?>
 							<?php the_post_thumbnail( 'large' ); ?>
 						<?php endif; ?>
 
-						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+						<?php the_title( sprintf( '<h2 class="shop-product-header"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 					</header><!-- .entry-header -->
 
@@ -38,7 +55,7 @@ get_header(); ?>
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
-
+		</div>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
--- themes/inhabitent-theme/taxonomy-product-type.php
+++ themes/inhabitent-theme/taxonomy-product-type.php
@@ -8,24 +8,37 @@
 get_header(); ?>
 
 <div id="primary" class="content-area">
+		
+	<header class="category-header">
+        <h1 class="front-titles">
+        	<?php single_term_title();?>
+        </h1>
+        
+        <p><?php the_archive_description();
+        ?> </p>
+    </header>
+
 	<main id="main" class="site-main" role="main">
 
+		<div class="category-archive-product">
+
 		<?php if ( have_posts() ) : ?>
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					single_term_title();?>
-				</h1>
-				<?php the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-				get_template_part( 'template-parts/content' );
-				?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="shop-grid-item">
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'large' ); ?>
+						<?php endif; ?>
+
+						<?php the_title( sprintf( '<h2 class="shop-product-header"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+					</header><!-- .entry-header -->
+
+				</article><!-- #post-## -->
+
 
 			<?php endwhile; ?>
 
@@ -36,7 +49,7 @@ get_header(); ?>
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
-
+		</div>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
