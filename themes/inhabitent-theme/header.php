cb6834979d40aab426895b868b8e3d7314ed5a82
Joseph-Lam <joseph.lam91@gmail.com>
2016-08-29 16:27:09 -0700

    changed header to become fixed positioned

--- themes/inhabitent-theme/header.php
+++ themes/inhabitent-theme/header.php
@@ -19,7 +19,8 @@
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
-
+			
+			<div class="header-fixed">
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<a href="<?php echo home_url()?>"><img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-tent.svg' ?>"></a>
@@ -31,12 +32,11 @@
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					
 					<?php get_search_form(); ?>
 
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
-
+</div>
 			<div id="content" class="site-content">
