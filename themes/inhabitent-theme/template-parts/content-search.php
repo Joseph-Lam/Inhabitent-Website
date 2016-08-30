eabfd55da800340738e0cf96019a94c40c38650c
Joseph-Lam <joseph.lam91@gmail.com>
2016-08-30 10:13:59 -0700

    added styling to search page

--- themes/inhabitent-theme/template-parts/content-search.php
+++ themes/inhabitent-theme/template-parts/content-search.php
@@ -8,17 +8,22 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	<header class="search-entry-header">
+		<?php the_title( sprintf( '<h2 class="search-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
+		<div class="search-comments">
 			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-summary">
+	<div class="search-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
+
+	<div class="search-button">
+		<a class="search-button-text" href="<?php the_permalink(); ?>">read more →</a>
+	</div>
+
 </article><!-- #post-## -->
